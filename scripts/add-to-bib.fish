36e390cb8464bdd142f999cfde462a58aa42df99
joopitz <jonas.opitz@live.de>
2023-02-14 09:19:09 +0100

    fix(scripts): library break for urls with =

--- scripts/add-to-bib.fish
+++ scripts/add-to-bib.fish
@@ -23,10 +23,10 @@ function main
     # # Construct id from url
     # remove trailing forward-slash, if it exists
     set url (echo $url | sed -e "s/\/\$//")
-    # set the id to the bottom-level domain, replacing colons with dots
-    set id (echo $url | sed -e "s/.*\/\([^\/]*\)\$/\1/" | sed -e "s/:/./g")
+    # set the id to the bottom-level domain, replacing colons and equals with dots
+    set id (echo $url | sed -e "s/.*\/\([^\/]*\)\$/\1/" | sed -e "s/[:=]/./g")
     # set the site to the top-level domain, replacing colons with dots
-    set site (echo $url | sed -e "s/https*:\/\/\([^\/]*\).*/\1/" | sed -e "s/:/./g")
+    set site (echo $url | sed -e "s/https*:\/\/\([^\/]*\).*/\1/" | sed -e "s/[:=]/./g")
 
     # append entry to the bibliography file
     echo -e "\n@misc{$site-$id,\n  title = {$title},\n  url = {$url},\n  author = {$author},\n}" >>$file
