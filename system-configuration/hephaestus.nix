111585b36ec21c8e422194f9f1a3d2b26b88dbce
joopitz <jonas.opitz@live.de>
2023-02-21 16:15:52 +0100

    minor(nixos): also auto-update agenix input

--- system-configuration/hephaestus.nix
+++ system-configuration/hephaestus.nix
@@ -66,7 +66,7 @@ in {
   system.autoUpgrade = {
     enable = true;
     flake = "/etc/nixos";
-    flags = [ "--update-input" "nixpkgs" "--update-input" "home-manager" ];
+    flags = [ "--update-input" "nixpkgs" "--update-input" "home-manager" "--update-input" "agenix" ];
   };
   boot = {
     tmpOnTmpfs = true;
